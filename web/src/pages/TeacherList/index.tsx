7ff2d2c45751b075adc0a8fea8f4e74b101733d8
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-08-03 21:57:39 -0300

    Criando primeiro componente PageHeader

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,24 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHeader from '../../Components/PageHeader';
+
+import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="" />
-          </Link>
-          <img src={logoImg} alt="" />
-        </div>
-
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+      <PageHeader />
     </div>
   );
 }
