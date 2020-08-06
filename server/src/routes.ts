4ec675e68f7510565503b173b3f0fe457cd8209d
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-08-05 21:05:03 -0300

    Detach routes in different files

---
+++ server/src/routes.ts
@@ -0,0 +1,10 @@
+import express, { text } from 'express';
+import ClassesController from './controllers/ClassesController';
+
+const routes = express.Router();
+const classesController = new ClassesController();
+
+
+routes.post('/classes', classesController.create);
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello There " });
-});
+app.use(routes);
 
 app.listen(3333);
